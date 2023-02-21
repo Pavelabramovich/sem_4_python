856a485ba427c2d949e6f78280f01874ef316a07
Pavel <apn20040510@gmail.com>
2023-02-22 01:45:54 +0300

    add lab main file

---
+++ Lab1/main.py
@@ -0,0 +1,29 @@
+from hello_world import print_hello_world
+from operations import operation
+from even_list import parse_even
+from number_input import type_input
+from number_input import list_input
+
+# 1 task
+print()
+
+print_hello_world()
+
+# 2 task
+print()
+
+x = type_input(float)
+y = type_input(float)
+op = input("Input operation ")
+
+try:
+    print(f"Answer is {operation(x, y, op)}")
+except NotImplementedError:
+    print(f"Not implemented operation")
+
+# 3 task
+print()
+
+int_lst = list_input(int)
+print(f"Answer is {parse_even(int_lst)}")
+
