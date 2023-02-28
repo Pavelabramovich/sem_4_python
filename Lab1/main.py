8924310cb7d976faccdbab3c94ca7d59fd91de1a
Pavel <apn20040510@gmail.com>
2023-02-28 23:14:30 +0300

    add if name = main

--- Lab1/main.py
+++ Lab1/main.py
@@ -4,26 +4,29 @@ from even_list import parse_even
 from number_input import type_input
 from number_input import list_input
 
-# 1 task
-print()
+if __name__ == "__main__":
+    # 1 task
+    print()
 
-print_hello_world()
+    print_hello_world()
+    
+    # 2 task
+    print()
 
-# 2 task
-print()
+    x = type_input(float)
+    y = type_input(float)
+    op = input("Input operation ")
 
-x = type_input(float)
-y = type_input(float)
-op = input("Input operation ")
+    try:
+        print(f"Answer is {operation(x, y, op)}")
+    except NotImplementedError:
+        print(f"Not implemented operation")
+    except ZeroDivisionError:
+        print(f"Division by zero")
 
-try:
-    print(f"Answer is {operation(x, y, op)}")
-except NotImplementedError:
-    print(f"Not implemented operation")
+    # 3 task
+    print()
 
-# 3 task
-print()
-
-int_lst = list_input(int)
-print(f"Answer is {parse_even(int_lst)}")
+    int_lst = list_input(int)
+    print(f"Answer is {parse_even(int_lst)}")
 
